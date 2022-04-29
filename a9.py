9c2e043f6625cecc733c64913d3a51c1b0a8acd8
2Jiwo <104637513+2Jiwo@users.noreply.github.com>
2022-04-30 01:14:25 +0900

    Add files via upload

---
+++ a9.py
@@ -0,0 +1,23 @@
+fahrenheit = 100
+celsius = fahrenheit-32
+celsius = celsius * 5
+celsius = celsius/9
+print(celsius)
+
+
+x = 0
+y = 0 
+x = int(input("정수 x를 입력하시오: "))
+y=int(input("정수 y를 입력하시오: "))
+sum = x + y
+print(sum)
+
+
+price = int(input("상품의 가격을 입력하시오: "))
+vat = price * 0.1
+print(vat)
+
+
+age = int(input("현재 나이를 입력하시오: "))
+age = age + 10
+print("10년 후면", age," "세가 되시는 군요")
